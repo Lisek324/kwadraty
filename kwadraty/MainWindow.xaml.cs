5b9c0da3567fa4bf4f9ab62b4577cb92ddf77051
student <student@s218-pc02>
2022-04-12 15:19:36 +0200

    Dodaj pliki projektów.

---
+++ kwadraty/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kwadraty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Checkbox1_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Checkbox2_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Checkbox3_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Checkbox4_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Checkbox5_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Checkbox6_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
